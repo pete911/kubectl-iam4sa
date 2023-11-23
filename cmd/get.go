4723dbf70b00ee2d551b6e85ed4cd05466ec5d63
Peter Reisinger <prreisin@amazon.co.uk>
2023-11-23 15:21:28 +0000

    add failed events to get command

--- cmd/get.go
+++ cmd/get.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"github.com/pete911/kubectl-iam4sa/internal/aws"
 	"github.com/pete911/kubectl-iam4sa/internal/k8s"
+	"github.com/pete911/kubectl-iam4sa/internal/out"
 	"github.com/spf13/cobra"
 	"log/slog"
 	"os"
+	"time"
 )
 
 var (
@@ -46,16 +48,23 @@ func runGetCmd(_ *cobra.Command, args []string) {
 		fmt.Printf("get IAM service accounts: %v\n", err)
 		os.Exit(1)
 	}
-	printGet(logger, awsClient, sas)
-}
 
-func printGet(logger *slog.Logger, awsClient aws.Client, sas []k8s.ServiceAccount) {
-	for _, sa := range sas {
-		printGetSa(logger, awsClient, sa)
+	if err := printGet(logger, awsClient, sas); err != nil {
+		fmt.Printf("print get: %v\n", err)
+		os.Exit(1)
 	}
 }
 
-func printGetSa(logger *slog.Logger, awsClient aws.Client, sa k8s.ServiceAccount) {
+func printGet(logger *slog.Logger, awsClient aws.Client, sas []k8s.ServiceAccount) error {
+	for _, sa := range sas {
+		if err := printGetSa(logger, awsClient, sa); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func printGetSa(logger *slog.Logger, awsClient aws.Client, sa k8s.ServiceAccount) error {
 	role, err := awsClient.GetIAMRole(sa.RoleName())
 	if err != nil {
 		logger.Error(fmt.Sprintf("get role for %s/%s service account: %v", sa.Namespace, sa.Name, err))
@@ -67,10 +76,11 @@ func printGetSa(logger *slog.Logger, awsClient aws.Client, sa k8s.ServiceAccount
 		logger.Error(fmt.Sprintf("get cluster oidc provider url: %v", err))
 	}
 
-	//events, err := awsClient.LookupEvents(sa.Namespace, sa.Name)
-	//if err != nil {
-	//	logger.Error(fmt.Sprintf("lookup %s/%s event: %v", sa.Namespace, sa.Name, err))
-	//}
+	events, err := awsClient.LookupEvents(sa.Namespace, sa.Name)
+	if err != nil {
+		logger.Error(fmt.Sprintf("lookup %s/%s event: %v", sa.Namespace, sa.Name, err))
+	}
+	failedEvents := events.FailedEvents()
 
 	fmt.Printf("Namespace: %s Name: %s\n", sa.Namespace, sa.Name)
 	fmt.Println("pods:")
@@ -87,15 +97,34 @@ func printGetSa(logger *slog.Logger, awsClient aws.Client, sa k8s.ServiceAccount
 		fmt.Println("  Assume Policy Document:")
 		jsonPrettyPrint(logger, role.AssumeRolePolicyDocument)
 	}
+
+	// if there are any failed events, lets print them
+	if len(failedEvents) != 0 {
+		fmt.Println("Failed Events:")
+		table := out.NewTable()
+		if err := table.AddRow("TIME", "CODE", "MESSAGE", "REQUEST ROLE", "ACTUAL ROLE"); err != nil {
+			return err
+		}
+		for i, event := range failedEvents {
+			// print max last 5 failed events
+			if i == 5 {
+				break
+			}
+			if err := table.AddRow(event.EventTime.Format(time.RFC3339), event.ErrorCode, event.ErrorMessage, event.RequestParameters.RoleArn, sa.IamRoleArn); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
 
 func jsonPrettyPrint(logger *slog.Logger, in string) {
-	var out any
-	if err := json.Unmarshal([]byte(in), &out); err != nil {
+	var inJson any
+	if err := json.Unmarshal([]byte(in), &inJson); err != nil {
 		logger.Error(err.Error())
 		return
 	}
-	b, err := json.MarshalIndent(out, "", "  ")
+	b, err := json.MarshalIndent(inJson, "", "  ")
 	if err != nil {
 		logger.Error(err.Error())
 		return
